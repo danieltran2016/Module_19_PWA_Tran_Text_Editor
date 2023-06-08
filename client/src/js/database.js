5ac26c81e5d5a616f6f096ec6c3ac7892fd5ff02
Danny Tran <danieltran2016@gmail.com>
2023-06-08 01:08:18 -0500

    finished get command using module 23

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
 //make connection to DB and which version we wnat to use 
 const jateDb = await openDB('jate',1);
@@ -33,7 +33,26 @@ console.log('data saved to database', result);
 
 
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+//Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET all from the database');
+
+  //create connnection from database and choose which version we want to use
+  const jateDB = await openDB('jate',1);
+
+  //create a new transaction and specify data abilities 
+  const tx = jateDB.transaction('jate','readonly');
+
+  //open the object store
+  const store = tx.objectStore('jate');
+
+  //use getAll method to get everything from database
+  const request = store.getAll();
+
+  //get confirmation of request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
